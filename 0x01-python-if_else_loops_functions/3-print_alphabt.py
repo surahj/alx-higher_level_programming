c7db6b7fcfb0259b6fbd2e5b8798642b62317742
surahj <odebodeopeyemi@gmail.com>
2022-05-31 16:08:55 -0700

    Fix.error

--- 0x01-python-if_else_loops_functions/3-print_alphabt.py
+++ 0x01-python-if_else_loops_functions/3-print_alphabt.py
@@ -1,6 +1,6 @@
 #!/usr/bin/python3
 for letter in range(97, 123):
     if chr(letter) == 'q' or chr(letter) == 'e':
-            continue
+        continue
 
     print("{}".format(chr(letter)), end="")
