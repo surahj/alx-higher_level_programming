758e8a838239dbb49ebccab431f0769b180b4753
surahj <odebodeopeyemi@gmail.com>
2022-05-31 14:59:52 -0700

    Fix.error

--- 0x01-python-if_else_loops_functions/1-last_digit.py
+++ 0x01-python-if_else_loops_functions/1-last_digit.py
@@ -2,8 +2,7 @@
 import random
 number = random.randint(-10000, 10000)
 if number < 0:
-    number *= -1
-    lastDigit = -(number % 10)
+    lastDigit = -((number * -1) % 10)
 else:
     lastDigit = number % 10
 
