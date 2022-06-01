edb0fd00999dfbe8212fb5a2f7ce465d34a17429
surahj <odebodeopeyemi@gmail.com>
2022-06-01 01:28:57 -0700

    Add 9-main.py file

---
+++ 0x01-python-if_else_loops_functions/9-print_last_digit.py
@@ -0,0 +1,9 @@
+#!/usr/bin/python3
+def print_last_digit(number):
+    lastDigit = number % 10
+
+    if number < 0:
+        lastDigit = ((number * -1) % 10)
+    
+    print("{}".format(lastDigit), end="")
+    return lastDigit
