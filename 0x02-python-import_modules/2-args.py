f69ffb9d38c28a08dc89f05a9191c758c4c3bab8
surahj <odebodeopeyemi@gmail.com>
2022-06-02 08:39:56 -0700

    Add args.py file

---
+++ 0x02-python-import_modules/2-args.py
@@ -0,0 +1,14 @@
+#!/usr/bin/python3
+import sys
+
+if __name__ == "__main__":
+    lenght = len(sys.argv)
+
+    if lenght < 2:
+        print("0 argumenst.")
+        sys.exit()
+
+    print("{} arguments:".format(lenght - 1))
+
+    for i in range(1, lenght):
+        print("{}: {}".format(i, sys.argv[i]))
