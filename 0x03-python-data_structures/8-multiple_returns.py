939a03e55aa86d0111c7e72bc45c18b2d62bf7f6
surahj <odebodeopeyemi@gmail.com>
2022-06-04 09:46:45 -0700

    Fix error

--- 0x03-python-data_structures/8-multiple_returns.py
+++ 0x03-python-data_structures/8-multiple_returns.py
@@ -1,9 +1,7 @@
 #!/usr/bin/python3
 
 def multiple_returns(sentence):
-    length = len(sentence)
-    first_char = sentence[0]
     if sentence == "":
         return (0, None)
 
-    return (length, first_char)
+    return (len(sentence), sentence[0])
