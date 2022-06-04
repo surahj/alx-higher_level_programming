fd9d7290ed7992e709ebfd59be572f8ca1b24ab2
surahj <odebodeopeyemi@gmail.com>
2022-06-04 03:52:20 -0700

    Add 5-no_c.py file

---
+++ 0x03-python-data_structures/test/5-main.py
@@ -0,0 +1,10 @@
+#!/usr/bin/env python3
+import sys
+
+sys.path.insert(0, '/alx/alx-higher_level_programming/0x03-python-data_structures')
+
+no_c = __import__('5-no_c').no_c
+
+print(no_c("Best School"))
+print(no_c("Chicago"))
+print(no_c("C is fun!"))
