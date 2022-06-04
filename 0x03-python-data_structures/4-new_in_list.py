0870459b4cddb352cffa0e3264a24fa4cb5bb70a
surahj <odebodeopeyemi@gmail.com>
2022-06-04 02:53:46 -0700

    Fix error

--- 0x03-python-data_structures/4-new_in_list.py
+++ 0x03-python-data_structures/4-new_in_list.py
@@ -6,7 +6,5 @@ def new_in_list(my_list, idx, element):
 
     new_list = my_list.copy()
 
-    for i in range(len(my_list) - 1):
-        if i == idx:
-            new_list[i] = element
-            return new_list
+    new_list[idx] = element
+    return new_list
