9c4b6ea412cb7f67c5c6cbcf02c9ec387d55c76a
surahj <odebodeopeyemi@gmail.com>
2022-06-10 01:02:26 -0700

    Fix error

--- 0x04-python-more_data_structures/7-update_dictionary.py
+++ 0x04-python-more_data_structures/7-update_dictionary.py
@@ -2,6 +2,5 @@
 
 def update_dictionary(a_dictionary, key, value):
 
-    a_dictionary['key'] = value
-
-    return (a_dictionary)
+    a_dictionary.update({key: value})
+    return (a_dictionary.copy())
