7f7af777f83fce7c0cb0515dc57856f1bc0c8bda
surahj <odebodeopeyemi@gmail.com>
2022-06-11 07:37:46 -0700

    Fix error

--- 0x04-python-more_data_structures/100-weight_average.py
+++ 0x04-python-more_data_structures/100-weight_average.py
@@ -4,10 +4,13 @@ def weight_average(my_list=[]):
     tuple_sum = 0
     weight_sum = 0
 
+    if len(my_list) == 0:
+        return (0)
+
     for number in my_list:
         tuple_sum += (number[0] * number[1])
         weight_sum += (number[1])
-        
+
     average = tuple_sum / weight_sum
 
-    return average
+    return (average)
