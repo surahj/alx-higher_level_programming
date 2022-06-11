9f32f6bd1f2e95790d36215209229ce00981d953
surahj <odebodeopeyemi@gmail.com>
2022-06-11 07:33:29 -0700

    Add 100-weight_average.py file

---
+++ 0x04-python-more_data_structures/100-weight_average.py
@@ -0,0 +1,13 @@
+#!/usr/bin/python3
+
+def weight_average(my_list=[]):
+    tuple_sum = 0
+    weight_sum = 0
+
+    for number in my_list:
+        tuple_sum += (number[0] * number[1])
+        weight_sum += (number[1])
+        
+    average = tuple_sum / weight_sum
+
+    return average
