d80e6b8fd2c10044c8594bcc890366c858efa404
surahj <odebodeopeyemi@gmail.com>
2022-06-28 07:51:39 +0100

    Update class

--- 0x08-python-more_classes/6-main.py
+++ 0x08-python-more_classes/6-main.py
@@ -2,9 +2,16 @@
 Rectangle = __import__('6-rectangle').Rectangle
 
 my_rectangle_1 = Rectangle(2, 4)
+my_rectangle_1.number_of_instances = 10
+print(my_rectangle_1)
+
+print("{:d} is my_rectangle_1.number_of_instances".format(my_rectangle_1.number_of_instances))
 my_rectangle_2 = Rectangle(2, 4)
+
 print("{:d} instances of Rectangle".format(Rectangle.number_of_instances))
 del my_rectangle_1
 print("{:d} instances of Rectangle".format(Rectangle.number_of_instances))
+
+print("****************{:d} instances of Rectangle".format(my_rectangle_2.number_of_instances))
 del my_rectangle_2
 print("{:d} instances of Rectangle".format(Rectangle.number_of_instances))
