704b6b50d1a32a709b450ad343777fe47f249d18
surahj <odebodeopeyemi@gmail.com>
2022-07-04 08:42:21 -0700

    Update

---
+++ 0x0A-python-inheritance/3-is_kind_of_class.py
@@ -0,0 +1,13 @@
+#!/usr/bin/python3
+""" checks for Exact same object """
+
+
+def is_same_class(obj, a_class):
+    """ return true if it is exact same object
+        otherwise false
+    """
+
+    if (isinstance(obj, a_class)):
+        return True
+
+    return False
