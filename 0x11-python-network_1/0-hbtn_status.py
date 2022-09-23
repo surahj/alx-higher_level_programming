fcb96b6145e134f6448c9900e8b84b7be268dbe8
surahj <odebodeopeyemi@gmail.com>
2022-09-23 05:05:35 -0700

    Fix error

--- 0x11-python-network_1/0-hbtn_status.py
+++ 0x11-python-network_1/0-hbtn_status.py
@@ -1,11 +1,11 @@
 #!/usr/bin/python3
 """Fetches https://intranet.hbtn.io/status."""
-from urllib.request import Request, urlopen
+import urllib.request
 
 
 if __name__ == "__main__":
-    request = Request("https://intranet.hbtn.io/status")
-    with urlopen(request) as response:
+    request = urllib.request.Request("https://intranet.hbtn.io/status")
+    with urllib.request.urlopen(request) as response:
         body = response.read()
         print("Body response:")
         print("\t- type: {}".format(type(body)))
--- 0x11-python-network_1/1-hbtn_header.py
+++ 0x11-python-network_1/1-hbtn_header.py
@@ -1,13 +1,14 @@
 #!/usr/bin/python3
-"""Fetches https://intranet.hbtn.io/status."""
+"""Displays the X-Request-Id header variable of a request to a given URL.
+Usage: ./1-hbtn_header.py <URL>
+"""
+import sys
 import urllib.request
 
 
 if __name__ == "__main__":
-    request = urllib.request.Request("https://intranet.hbtn.io/status")
+    url = sys.argv[1]
+
+    request = urllib.request.Request(url)
     with urllib.request.urlopen(request) as response:
-        body = response.read()
-        print("Body response:")
-        print("\t- type: {}".format(type(body)))
-        print("\t- content: {}".format(body))
-        print("\t- utf8 content: {}".format(body.decode("utf-8")))
+        print(dict(response.headers).get("X-Request-Id"))
