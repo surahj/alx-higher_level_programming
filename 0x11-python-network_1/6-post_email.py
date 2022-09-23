503f891f08d18664152526b28cade3f955fcd02f
surahj <odebodeopeyemi@gmail.com>
2022-09-23 07:04:56 -0700

    Fix error

--- 0x11-python-network_1/6-post_email.py
+++ 0x11-python-network_1/6-post_email.py
@@ -1,7 +1,10 @@
 #!/usr/bin/python3
 # -*- coding: utf-8 -*-
 """
-takes in a URL and an email address, sends a POST request to the passed URL with the email as a parameter, and finally displays the body of the response.
+takes in a URL and an email address,
+sends a POST request to the passed URL
+with the email as a parameter, and
+finally displays the body of the response.
 """
 from requests import post
 from sys import argv
